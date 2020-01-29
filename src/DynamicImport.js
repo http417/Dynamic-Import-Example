f83af77851e218503c06a6d09bb4babcc44b3147
Christopher Cheng <http300-github@yahoo.com>
2020-01-29 13:20:02 -0600

    Clean up

--- src/DynamicImport.js
+++ src/DynamicImport.js
@@ -1,9 +1,6 @@
 import React, {useState} from "react";
 
-const DynamicImport = ({path}) => {
-  console.log('path is : ', path);
-  const Fallback = () => <div>...Loading</div>
-
+const DynamicImport = ({path, Fallback}) => {
   const [module, setModule] = useState(<Fallback />);
   import(`./${path}`).then(module => module.default)
   .then(module => new Promise(resolve => setTimeout(()=> setModule(module), 1000)))
